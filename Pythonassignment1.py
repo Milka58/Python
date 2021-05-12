6777cb7746f9d9afe939948763bb98ec7eb12a6e
Milka Gikunju <32275220+Milka58@users.noreply.github.com>
2021-05-12 00:07:14 -0500

    Assignment 1

---
+++ Pythonassignment1.py
@@ -0,0 +1,11 @@
+# Enter first and last name
+names = input("Milka Gikunju")
+print(names)
+print()
+# Enter your goals for this class
+print("Leverage Python to get better at my current and future roles")
+goals = input("Skilled python programmer")
+print(goals)
+
+
+
